6a13cac65f81d70aa897aab170a7763375c837a5
Michael Caron <michael.r.caron@gmail.com>
2019-12-02 06:16:56 -0600

    First solution

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace advent_of_code_2019
 {
@@ -6,7 +7,29 @@ namespace advent_of_code_2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalculateFuel(args[0]);
+        }
+
+        private static void CalculateFuel(string inputFile)
+        {
+            if (! File.Exists(inputFile))
+            {
+                Console.WriteLine("Can't work without input!");
+            }
+
+            using (var inputReader = new StreamReader(
+                File.Open(inputFile, FileMode.Open))
+            )
+            {
+                while(!inputReader.EndOfStream) 
+                {
+                    int mass = Int32.Parse(inputReader.ReadLine().Trim());
+                    
+                    int fuel = mass / 3 - 2;
+
+                    Console.WriteLine($"Fuel for Mass({mass}): {fuel}");
+                }
+            }
         }
     }
 }
